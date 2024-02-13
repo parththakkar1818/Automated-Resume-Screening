fdb1d4a2b46026637c9a167e22a8e359d15c3c08
Vyom Padalia <104118297+rarevyom09@users.noreply.github.com>
2024-02-13 12:07:55 +0530

    new with job desc

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI, UploadFile, File, Query
+from fastapi import FastAPI, UploadFile, File, Form
 from PyPDF2 import PdfReader
 from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
-from sentence_transformers import SentenceTransformer , util
-from sklearn.metrics.pairwise import cosine_similarity
-# from nltk.translate import paraphrase
+from sentence_transformers import SentenceTransformer, util
 import spacy
-from typing import List  # Import List type from the typing module
-
+from typing import List
 
 app = FastAPI()
 
@@ -19,45 +16,36 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Load the Sentence Transformer model
-# model = SentenceTransformer("all-MiniLM-L6-v2")
 model = SentenceTransformer("all-mpnet-base-v2")
-
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World18"}
+    return {"Hello": "World"}
 
 @app.post("/extract_and_sort")
-async def extract_and_sort(resume_files: List[UploadFile] = File(...)):
+async def extract_and_sort(job_description: str = Form(...), resume_files: List[UploadFile] = File(...)):
     scores = []
 
     for file in resume_files:
         if file.filename.endswith('.pdf'):
-            # Read the PDF file
             contents = await file.read()
-            reader = PdfReader(BytesIO(contents))  # Pass BytesIO object to PdfReader
-            
-            # Extract text from the first page
+            reader = PdfReader(BytesIO(contents))
             page = reader.pages[0]
             text = page.extract_text()
             nlp = spacy.load('en_core_web_sm')
             cleaned_text = ' '.join([token.text for token in nlp(text) if not token.is_stop])
-            # print("cleaned text: ",cleaned_text)
 
-            job_description= "i want python and react developer"
-            # print(paraphrase.phrase("I want a Python and React developer."))
-
-
-            emb_1 = model.encode(text)
+            emb_1 = model.encode(cleaned_text)
             emb_2 = model.encode(job_description)
-            # similarity = util.cos_sim(emb_1, emb_2)
-            similarity = round(util.pytorch_cos_sim(emb_1, emb_2).item() , 3)  # Convert tensor to Python float
+            print(cleaned_text)
+            print(text)
+            print(job_description)
             
-             # Append score and filename to scores list
+            similarity = round(util.pytorch_cos_sim(emb_1, emb_2).item(), 3)
+
             scores.append({"filename": file.filename, "score": similarity})
         else:
             return {"error": "Uploaded file is not a PDF"}
-    
+
     sorted_scores = sorted(scores, key=lambda x: x["score"], reverse=True)
     return sorted_scores
